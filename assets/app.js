ce785be5d33d0db82e910ad01efa8820a33e7efe
ichoi21 <ihchoi@gmail.com>
2020-05-08 23:49:51 -0700

    added vars to JS, defined divs

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,8 @@
-var APIid = "&appid=987370c9088242014b673e9c345ee3d9";
-var units = "&units=imperial";
-var API = "http://api.openweathermap.org/data/2.5/weather?";
-
 $(document).ready(function () {
+  var APIid = "&appid=987370c9088242014b673e9c345ee3d9";
+  var units = "&units=imperial";
+  var API = "http://api.openweathermap.org/data/2.5/weather?";
+
   $("#btnSearch").on("click", function (e) {
     e.preventDefault();
 
@@ -15,23 +15,31 @@ $(document).ready(function () {
       dataType: "json",
     }).then(function (res) {
       console.log(res);
-      temp = res.main.temp;
-      console.log((humidity = res.main.humidity));
-      var lat = res.coord.lat;
-      var lon = res.coord.lon;
-      var weather = res.weather.main;
-      var windSpeed = res.wind.speed;
-      var icon = res.weather.icon;
-      var countryCode = res.sys.country;
+      var name = res.name;
+      var date = new Date(res.dt * 1000).toLocaleDateString("en-US");
+      var icon = res.weather[0].icon;
+      var weather = res.weather.main.description;
+      var temp = Math.floor(res.main.temp);
       var humidity = res.main.humidity;
-      var date = new Date(results.dt * 1000).toLocaleDateString("en-US");
+      var windSpeed = res.wind.speed;
 
-      $("#cityName").text(name);
-      $("#temperature").text(temp);
-      $("#humidity").text(humidity + "%");
+      var countryCode = res.sys.country;
+
+      $("#cityName").text(name + " (" + date + ") ");
+      $("#icon").attr(
+        "src",
+        "http://openweathermap.org/img/wn/" + icon + ".png"
+      );
+      $("#weather").html(weather);
+      $("#temperature").html("<b>Temperature: </b>" + temperature + " °F");
+      $("#humidity").html("<b>Humidity: </b>" + humidity + "%");
+      $("#windSpeed").html("<b>Wind Speed: </b>" + windSpeed + " MPH");
     });
   });
   //City UV
+
+  var lat = res.coord.lat;
+  var lon = res.coord.lon;
 
   $.ajax({
     type: "GET",
