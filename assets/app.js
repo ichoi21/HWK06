c4f8ac4af5c49441e11e9baaa723ceb5b94b1df2
ichoi21 <ihchoi@gmail.com>
2020-05-08 22:45:38 -0700

    back up

--- assets/app.js
+++ assets/app.js
@@ -8,12 +8,47 @@ $(document).ready(function () {
     //City Temp
     $.ajax({
       type: "GET",
-      url: `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch}&appid=${APIid}&units=imperial`,
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${APIid}&units=imperial`,
       dataType: "json",
     }).then(function (res) {
       console.log(res);
-      kelvin = res.main.temp_max;
-      $("#weather-display").prepend(`<h1>The temperature is ${kelvin} *F</h1>`);
+      temp = res.main.temp;
+      console.log((humidity = res.main.humidity));
+      var lat = res.coord.lat;
+      var lon = res.coord.lon;
+      weather = res.weather.main;
+      weatherdes = res.weather.description;
+      icon = res.weather.icon;
+      var countryCode = res.sys.country;
+      humidity = res.main.humidity;
+      // $("#weather-display").text(`<h3>The temperature is ${temp} *F</h3>
+      // <br />
+      // <h3>${weather} with ${weatherdes}</h3>
+      // <br />
+      // <h3>${icon}</h3>`);
+      $("#weather-display").prepend(
+        `<h3>The temperature is ${temp} with ${weatherdes} ${icon}</h3>`
+      );
+    });
+    //City UV
+
+    $.ajax({
+      type: "GET",
+      url: `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lat=${lon}&appid=${APIid}`,
+      dataType: "json",
+    }).then(function (res) {
+      console.log(res);
+
+      $("#weather-display").text(`<h4>Coordinates is ${lat}, ${lon}</h4>`);
+    });
+    //City Forecast
+
+    $.ajax({
+      type: "GET",
+      url: `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch},${countryCode}&appid=${APIid}&units=imperial`,
+      dataType: "json",
+    }).then(function (res) {
+      console.log(res);
     });
   });
 });
